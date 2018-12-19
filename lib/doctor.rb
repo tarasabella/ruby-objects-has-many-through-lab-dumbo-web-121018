4dbea60e403c682ac3989920d4e7e92336a7438b
Tara Sabella <tarasabella@gmail.com>
2018-12-19 13:59:28 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,62 @@
+class Doctor
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def patients
+    self.appointments.collect {|appointment| appointment.patient}
+  end
+end
+# ///////////////////////////////////
+class Patient
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def doctors
+    self.appointments.collect {|appointment| appointment.doctor}
+  end
+end
+# ////////////////////////////////////
+# ////////////////////////////////////
+# ////////////////////////////////////
+
+doctor_smith = Doctor.new("John Smith")
+Doctor.new("Martha Jones")
+doctor_smith.appointments # should be empty array
+
+doctor_who = Doctor.new("The Doctor")
+doctor_who.name #=> Should be: "The Doctor"
+
+
+appointment = Appointment.new("Friday, January 32nd", doctor_who)
+doctor_who.add_appointment(appointment) #=> 
+
+martin = Patient.new("Martin Jones")
+martin.add_appointment(appointment)
+      
+doctor_who.add_appointment(appointment)
+doctor_who.patients.include?(martin)
