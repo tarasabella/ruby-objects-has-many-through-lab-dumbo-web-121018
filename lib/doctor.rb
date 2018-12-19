2bab3cc4367cde5ecd5fce46fd840100fd62e3ad
Tara Sabella <tarasabella@gmail.com>
2018-12-19 13:59:56 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,28 +18,8 @@ class Doctor
     self.appointments.collect {|appointment| appointment.patient}
   end
 end
-# ///////////////////////////////////
-class Patient
-  attr_accessor :name
 
-  def initialize(name)
-    @name = name
-    @appointments = []
-  end
 
-  def add_appointment(appointment)
-    @appointments << appointment
-    appointment.patient = self
-  end
-
-  def appointments
-    @appointments
-  end
-
-  def doctors
-    self.appointments.collect {|appointment| appointment.doctor}
-  end
-end
 # ////////////////////////////////////
 # ////////////////////////////////////
 # ////////////////////////////////////
